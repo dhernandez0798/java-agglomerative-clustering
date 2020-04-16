b815f199c4f58395cd1a356771273bc9a4c63d9d
Daniel Hernández <dhernandez0798@gmail.com>
2020-04-16 18:48:46 +0200

    refactoring

--- src/com/github/dhernandez0798/AgglomerativeClustering.java
+++ src/com/github/dhernandez0798/AgglomerativeClustering.java
@@ -1,30 +1,46 @@
 package com.github.dhernandez0798;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class AgglomerativeClustering {
-    List<Cluster> clusters = new ArrayList<>();
 
-    public AgglomerativeClustering(List<Cluster> clusters) {
-        this.clusters.addAll(clusters);
-    }
-
-    public AgglomerativeClustering(double[] clusters) {
+    // We transform a simple array to a list here.
+    public static void proccess(double[] clusters, int clustersAmount, LinkageCriterion linkageCriterion) {
+        List<Cluster> tempClusters = new ArrayList<>();
         for (double cluster : clusters) {
-            this.clusters.add(new Cluster(cluster));
+            tempClusters.add(new Cluster(cluster));
         }
+        process(tempClusters, clustersAmount, linkageCriterion);
     }
 
-    public void process(LinkageCriterion linkageCriterion) {
-        
+    public static void process(List<Cluster> clusters, int clustersAmount, LinkageCriterion linkageCriterion) {
+        int[] clusterResult;
+        while (clustersAmount < clusters.size()) {
+            clusterResult = agglomerate(clusters, linkageCriterion);
+
+            // Store the clusters so we can remove them later by element, not by position, as it will change.
+            Cluster cluster1 = clusters.get(clusterResult[0]);
+            Cluster cluster2 = clusters.get(clusterResult[1]);
+            Cluster newCluster = Cluster.mergeClusters(cluster1, cluster2);
+            clusters.remove(cluster1);
+            clusters.remove(cluster2);
+            clusters.add(newCluster);
+        }
+        System.out.println(Arrays.toString(clusters.toArray()));
     }
 
-    private ClusterResult agglomerate(LinkageCriterion linkageCriterion) {
+    private static int[] agglomerate(List<Cluster> clusters, LinkageCriterion linkageCriterion) {
         double distance = Double.POSITIVE_INFINITY;
+
+        // To store the position in array of he closest pair.
         int[] closestPair = new int[]{Integer.MIN_VALUE, Integer.MIN_VALUE};
+
         for (int i = 0; i < clusters.size(); i++) {
             for (int j = 0; j < clusters.size(); j++) {
+
+                // Do not calculate distance if i == j. It means they're the same item and distance will be 0.
                 if (j != i) {
                     double tempDistance = linkageCriterion.calculate(clusters.get(i), clusters.get(j));
                     if (tempDistance < distance) {
@@ -35,10 +51,6 @@ public class AgglomerativeClustering {
                 }
             }
         }
-        Cluster mergedCluster = Cluster.mergeClusters(
-                clusters.get(closestPair[0]),
-                clusters.get(closestPair[1])
-        );
-        return new ClusterResult(mergedCluster, closestPair);
+        return closestPair;
     }
 }
--- src/com/github/dhernandez0798/Main.java
+++ src/com/github/dhernandez0798/Main.java
@@ -1,57 +1,9 @@
 package com.github.dhernandez0798;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
         double[] inputArray = {5, 88, 326.4, 617, 8412, 71824, 412, 2};
-        List<Cluster> input = new ArrayList<>();
-        for (double element : inputArray) {
-            input.add(new Cluster(element));
-        }
-        /*while (input.size() > ) {
-            int[] closestPair = mergeInput(input);
-            Cluster c1 = input.get(closestPair[0]);
-            Cluster c2 = input.get(closestPair[1]);
-            Cluster newCluster = Cluster.mergeClusters(c1, c2);
-            input.remove(c1);
-            input.remove(c2);
-            input.add(newCluster);
-        }*/
-        for (int i = 0; i < 3; i++) {
-            int[] closestPair = mergeInput(input);
-            Cluster c1 = input.get(closestPair[0]);
-            Cluster c2 = input.get(closestPair[1]);
-            Cluster newCluster = Cluster.mergeClusters(c1, c2);
-            input.remove(c1);
-            input.remove(c2);
-            input.add(newCluster);
-        }
-        for (Cluster cluster: input) {
-            System.out.println(cluster);
-        }
-    }
-
-    public static int[] mergeInput(List<Cluster> input) {
-        double minDistance = Double.POSITIVE_INFINITY;
-        LinkageCriterion linkageCriterion = new SingleLinkageCriterion(new EuclideanDistance());
-        // To store the position in array of he closest pair.
-        int[] closestPair = new int[]{Integer.MIN_VALUE, Integer.MIN_VALUE};
-        for (int i = 0; i < input.size(); i++) {
-            for (int j = 0; j < input.size(); j++) {
-                // Do not calculate distance if i == j. It means they're the same item and distance will be 0.
-                if (j != i) {
-                    double distance = linkageCriterion.calculate(input.get(i), input.get(j));
-                    if (distance < minDistance) {
-                        minDistance = distance;
-                        closestPair[0] = i;
-                        closestPair[1] = j;
-                    }
-                }
-            }
-        }
-        return closestPair;
+        AgglomerativeClustering.proccess(inputArray, 4, new SingleLinkageCriterion(new EuclideanDistance()));
     }
 }
